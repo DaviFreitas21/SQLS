5702858f58fb0f4895acdd4f3ff135aa4407ec8d
Davi Freitas <df300409@gmail.com>
2026-03-15 16:28:47 -0300

    Insert, Update, Delete upd

--- bdEstoque.sql
+++ bdEstoque.sql
@@ -17,18 +17,18 @@ CREATE TABLE tbVenda(
 	idVenda INT  PRIMARY  KEY  IDENTITY (1,1),
 	dataVenda SMALLDATETIME NOT NULL,
 	valorTotalVenda SMALLMONEY NOT NULL,
-	idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente) NOT NULL,
+	idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente) NOT NULL
 )
 
 CREATE TABLE tbFabricante(
 	idFabricante INT PRIMARY KEY IDENTITY(1,1),
-	nomeFabricante VARCHAR (40) NOT NULL,
+	nomeFabricante VARCHAR (40) NOT NULL
 )
 
 CREATE TABLE tbFornecedor(
 	idFornecedor INT PRIMARY KEY IDENTITY (1,1),
 	nomeFornecedor VARCHAR (60) NOT NULL,
-	contatoFornecedor VARCHAR (50) NOT NULL,
+	contatoFornecedor VARCHAR (50) NOT NULL
 )
 
 CREATE TABLE tbProduto(
@@ -37,7 +37,7 @@ CREATE TABLE tbProduto(
 	valorProduto SMALLMONEY NOT NULL,
 	qtdProduto INT NOT NULL,
 	idFabricante INT FOREIGN KEY REFERENCES tbFabricante(idFabricante) NOT NULL,
-	idFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(idFornecedor) NOT NULL,
+	idFornecedor INT FOREIGN KEY REFERENCES tbFornecedor(idFornecedor) NOT NULL
 )
 
 CREATE TABLE tbItensVenda(
@@ -45,8 +45,67 @@ CREATE TABLE tbItensVenda(
 	qtdItensVenda INT NOT NULL,
 	subTotalItensVenda SMALLMONEY NOT NULL,
 	idVenda INT FOREIGN KEY REFERENCES tbVenda(idVenda) NOT NULL,
-	idProduto INT FOREIGN KEY REFERENCES tbProduto(idProduto) NOT NULL, 
+	idProduto INT FOREIGN KEY REFERENCES tbProduto(idProduto) NOT NULL
 )
 
 INSERT INTO tbCliente (nomeCliente,cpfCliente,emailCliente,sexoCliente,dataNascCliente)
-VALUES ('Armando José Santana', '12345678900','armandojsantana@outlook.com','m','21-02-1961'),
+VALUES ('Armando José Santana', '12345678900','armandojsantana@outlook.com','m','21/02/1961'),
+('Sheila Carvalho Leal','45678909823','scarvalho@ig.com.br', 'f', '13/09/1978'),
+('Carlos Henrique Souza','76598278299','chenrique@ig.com.br','m','08/09/1981'),
+('Maria Aparecida Souza','87365309899','mapdasouza@outlook.com','f','07/07/1962'),
+('Adriana Nogueira Silva','76354309388','drica1977@ig.com.br','f','09/04/1977'),
+('Paulo Henrique Silva','87390123111','phsilva80@hotmail.com','m','02/02/1987')
+
+INSERT INTO tbFabricante(nomeFabricante)
+VALUES ('Unilever'),
+('P&G'),
+('Bunge')
+
+
+INSERT INTO tbFornecedor (nomeFornecedor, contatoFornecedor)
+VALUES ('Atacadão','Carlos Santos'),
+('Assaí','Maria Stella'),
+('Roldão','Paulo César')
+
+INSERT INTO tbProduto (descProduto, valorProduto, qtdProduto, idFabricante, idFornecedor)
+VALUES ('Amaciante Downy',5.76,1500,2,1),
+('Amaciante Comfort',5.45,2300,1,1),
+('Sabão em pó OMO',5.99,1280,1,2),
+('Margarina Qualy',4.76,2500,3,1),
+('Salsicha Hot Dog Sadia',6.78,2900,3,2),
+('Mortadela Perdigão',2.50,1200,3,3),
+('Hamburguer Sadia',9.89,1600,3,1),
+('Fralda Pampers',36.00,340,2,3),
+('Xampu Seda',5.89,800,1,2),
+('Condicionador Seda',6.50,700,1,3)
+
+INSERT INTO tbVenda(dataVenda,valorTotalVenda,idCliente)
+VALUES ('01/02/2014',4500.00,1),
+('03/02/2014',3400.00,1),
+('10/02/2014',2100.00,2),
+('15/02/2014',2700.00,3),
+('17/03/2014',560.00,3),
+('09/04/2014',1200.00,4),
+('07/05/2014',3500.00,5),
+('07/05/2014',3400.00,1),
+('05/05/2014',4000.00,2)
+
+INSERT INTO tbItensVenda (qtdItensVenda, subTotalItensVenda, idVenda, idProduto)
+VALUES (200,1500.00,1,1),
+(300,3000.00,1,2),
+(120,1400.00,2,4),
+(200,1000.00,2,2),
+(130,1000.00,2,3),
+(200,2100.00,3,5),
+(120,1000.00,4,4),
+(450,700.00,4,5),
+(200,560.00,5,5),
+(200,600.00,6,7),
+(300,600.00,6,6)
+
+SELECT * FROM tbCliente
+SELECT * FROM tbFabricante
+SELECT * FROM tbFornecedor
+SELECT * FROM tbItensVenda
+SELECT * FROM tbProduto
+SELECT * FROM tbVenda
