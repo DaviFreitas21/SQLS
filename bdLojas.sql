6aadb6309cd8f947d59657ed22ef185c4f1fdd7c
Davi Freitas <df300409@gmail.com>
2026-03-23 12:07:42 -0300

    Upd exercicio 2

--- bdLojas.sql
+++ bdLojas.sql
@@ -31,7 +31,6 @@ CREATE TABLE tbProduto(
 	nomeProduto Varchar (30) NOT NULL,
 	precoProduto MONEY NOT NULL,
 	dataEntradaProduto SMALLDATETIME NOT NULL,
-	idFuncionario INT FOREIGN KEY REFERENCES tbFuncionario(idFuncionario),
 	idFabricante INT FOREIGN KEY REFERENCES tbFabricante(idFabricante) NOT NULL,
 )
 
@@ -40,7 +39,7 @@ CREATE TABLE tbVenda(
 	dataVenda SMALLDATETIME NOT NULL,
 	idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente) NOT NULL,
 	idVendedor INT FOREIGN KEY REFERENCES tbVendedor(idVendedor) NOT NULL,
-	totalVendas INT NOT NULL,
+	totalVenda MONEY NOT NULL,
 )
 
 CREATE TABLE tbItensVenda(
@@ -56,3 +55,29 @@ VALUES ('Malwee'),
 ('Marisol'),
 ('Cia da Malha')
 
+INSERT INTO tbProduto (nomeProduto,precoProduto, dataEntradaProduto, idFabricante)
+VALUES ('Conjunto Pijama', 119.99 , '16/04/2024', 1),
+('Camiseta Licenciada do Palmeiras', 99.99 ,'25/10/2023', 3),
+('Camiseta Licenciada do Flamengo', 99.99, '22/11/2025', 2),
+('Calça	Jogador Rasgada', 139.99, '04/04/2025', 2),
+('Chinelo de Borracha', 29.99, '07/09/2025', 1),
+('Jaqueta Puffer', 219.99 ,'06/07/2025', 3),
+('Shorts Jeans', 89.99, '02/02/2024', 2),
+('Calça Legging', 64.99, '19/05/2022', 2),
+
+INSERT INTO tbCliente(nomeCliente, idadeCliente)
+VALUES ('Henrique Negri', 16),
+('Thiago Borges', 16),
+('Aline Cordeiro', 67),
+('Clodoaldo Bastos', 58),
+('Kaique Candido', 16)
+
+INSERT INTO tbVendedor(nomeVendedor)
+VALUES ('Ryan Albuquerque'),
+('Rayssa Santos')
+
+INSERT INTO tbVenda( dataVenda, idCliente, idVendedor, totalVenda)
+VALUES ('11/12/2025', 1, 1, 204.98),
+('18/01/2026', 2, 2, 169.98),
+('23/03/2026', 3, 2, 129.98),
+('15/03/2024', 4, 1, 189.98)
